8c65591459aedc94c6a7dd68646b0ffaf8168d99
Anand-Bhakthavatsala <35788456+Anand-Bhakthavatsala@users.noreply.github.com>
2021-04-21 20:20:16 +0530

    commit

---
+++ MavenDemo2/src/test/java/MavenDemo2/MavenDemo2/Demo1.java
@@ -0,0 +1,14 @@
+package MavenDemo2.MavenDemo2;
+
+public class Demo1 {
+
+	public Demo1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome");
+	}
+
+}
