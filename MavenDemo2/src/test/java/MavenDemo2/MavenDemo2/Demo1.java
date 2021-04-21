740f4319dc7f40860f5861da20d1bbb8f27636df
Anand-Bhakthavatsala <35788456+Anand-Bhakthavatsala@users.noreply.github.com>
2021-04-21 20:26:18 +0530

    commit

--- MavenDemo2/src/test/java/MavenDemo2/MavenDemo2/Demo1.java
+++ MavenDemo2/src/test/java/MavenDemo2/MavenDemo2/Demo1.java
@@ -9,6 +9,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome");
+		System.out.println("Additional line");
 	}
 
 }
